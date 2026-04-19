872817b37600a218dc8b4c747c0d7540f3d90ea3
Sonny <sonnyasdev@gmail.com>
2026-04-19 05:09:52 +0200

    feat(section_title): update layout and styling for SectionTitle component

--- inertia/components/section_title.tsx
+++ inertia/components/section_title.tsx
@@ -3,12 +3,12 @@ interface SectionTitleProps {
 }
 
 export const SectionTitle = ({ title }: Readonly<SectionTitleProps>) => (
-	<div className="mb-8 grid grid-cols-[auto_1fr] items-center gap-3 sm:gap-4">
+	<div className="relative w-full mb-12">
 		<span
-			className="relative -bottom-1/2 h-0.5 w-8 rounded-full bg-gray-900 dark:bg-white/90"
+			className="absolute -bottom-1/4 h-0.5 w-8 rounded-full bg-gray-900 dark:bg-white/90"
 			aria-hidden
 		/>
-		<h2 className="text-lg text-center uppercase tracking-[0.25rem] text-gray-500 dark:text-gray-100">
+		<h2 className="text-lg text-center uppercase tracking-[0.25rem] text-gray-800 dark:text-gray-100">
 			{title}
 		</h2>
 	</div>
