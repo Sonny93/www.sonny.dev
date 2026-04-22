8e5fca167b87c88e2d3c23ded1b6975b55bc4ebb
Sonny <sonnyasdev@gmail.com>
2026-04-22 23:24:27 +0200

    fix(layouts): standardize width class for layout components

--- inertia/layouts/default_layout.tsx
+++ inertia/layouts/default_layout.tsx
@@ -5,7 +5,7 @@ import { Navbar } from '~/components/navigation/navbar';
 export const DefaultLayout = ({ children }: React.PropsWithChildren) => (
 	<BaseLayout>
 		<div className="flex min-h-dvh flex-col bg-gray-50 dark:bg-gray-900">
-			<div className="max-w-3xl mx-auto flex flex-1 flex-col gap-6 p-4">
+			<div className="w-3xl mx-auto flex flex-1 flex-col gap-6 p-4">
 				<Navbar />
 				<main
 					className="flex flex-col gap-16 py-10 sm:gap-20 sm:py-16"
